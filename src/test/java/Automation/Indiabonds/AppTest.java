9b73634e5f91341a6714dcc41cf4fb180d0bd04c
pankaj <kamblepankaj761@gmail.com>
2023-06-18 01:10:41 +0530

    test

--- src/test/java/Automation/Indiabonds/AppTest.java
+++ src/test/java/Automation/Indiabonds/AppTest.java
@@ -27,11 +27,11 @@ public class AppTest
     {
         assertTrue( true );
         System.out.println("test1");
-        System.out.println("test2");
+        System.out.println("test2"); 
         System.out.println("test3");
         System.out.println("test4");
         System.out.println("test5");
-        System.out.println("test6");
+    
 
        
     }
